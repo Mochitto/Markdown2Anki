639eacd838a59647ec35bf4a3de74fef9f7265d2
Mochitto <mochittodeveloper@gmail.com>
2022-12-27 00:47:38 +0100

    Feat: added support for config file for custom file paths and behavior

--- script_MD_to_anki/config_handle.py
+++ script_MD_to_anki/config_handle.py
@@ -7,6 +7,18 @@ CURRENT_DIR = os.getcwd()
 CURRENT_TIME = datetime.datetime.now()
 
 # From the config file
+def append_base_path_if_relative(file_path: str):
+    global BASE_PATH
+    if not os.path.isabs(file_path):
+        return os.path.join(BASE_PATH, file_path)
+    return file_path
+
+def append_outdir_if_relative(file_path:str):
+    global OUT_FOLDER
+    if not os.path.isabs(file_path):
+        return os.path.join(OUT_FOLDER, file_path)
+    return file_path
+
 config = configparser.ConfigParser()
 config.read('config.ini')
 
@@ -14,12 +26,19 @@ BASE_PATH = config["BASEPATH"]["base_path"]
 if not BASE_PATH:
     BASE_PATH = CURRENT_DIR
 
-MD_INPUT_FILE = config["PATHS"]["md_input_file"]
-OUT_FOLDER = config["PATHS"]["out_folder"]
+MD_INPUT_FILE = append_base_path_if_relative(config["INFILES"]["md_input_file"])
 
+OUT_FOLDER = append_base_path_if_relative(config["FOLDERS"]["out_folder"])
+
+RESULT_FILE = append_outdir_if_relative(config["OUTFILES"]["anki_csv_file"])
+BAD_CARDS_FILE = append_outdir_if_relative(config["OUTFILES"]["failed_cards_file"])
+LOG_FILE = append_outdir_if_relative(config["OUTFILES"]["log_file"])
+
+FAST_FORWARD = config["BEHAVIOR"]["fast_forward"]
 # Set-up
-os.makedirs(os.path.join(BASE_PATH, OUT_FOLDER), exist_ok=True) # Build out folder if missing
+os.makedirs(OUT_FOLDER, exist_ok=True) # Build out folder if missing
 
-def append_base_path_if_relative(config_path: str):
-    if not os.path.isabs(config_path):
-        
+
+
+
+
