82ab967968828499c584e2bc1932d98022bd568e
Mochitto <mochittodeveloper@gmail.com>
2023-03-01 12:35:00 +0100

    Refactor: fixed classes names

---
+++ tests/integration/md_2_anki/process_clozes/test_clozes_processing.py
@@ -0,0 +1,5 @@
+from markdown2anki.md_2_anki.process_clozes import process_clozes as clozes
+
+class TestGetClozes:
+
+    def 
--- tests/unit_tests/md_2_anki/process_clozes/test_replace_cloze_text_with_hashes.py
+++ tests/unit_tests/md_2_anki/process_clozes/test_replace_cloze_text_with_hashes.py
@@ -1,6 +1,6 @@
 from markdown2anki.md_2_anki.process_clozes import process_clozes as clozes
 
-class TestGetClozes:
+class TestReplaceClozeTextWithHashes:
 
     def test_basic_case(self):
         content = (
