bf61b111fec31201202c43bf4125e6aae2699cb9
Mochitto <mochittodeveloper@gmail.com>
2023-03-11 13:45:05 +0100

    Feat: Added bundler script for themes and updated dependencies

---
+++ Cards_type/esbuild_themes.js
@@ -0,0 +1,19 @@
+// This script is meant to bundle together CSS themes
+// Which are divided in Highlight themes and UI themes
+// This is so that you could, potentially, have different
+// Themes between UI and Code highlighting.
+import esbuild from 'esbuild';
+import path from "path";
+
+// Get the file path and filename from command line arguments
+const [themePath] = process.argv.slice(2);
+const fileName = path.basename(themePath)
+
+// Bundle and minimize the CSS files using esbuild with csso plugin
+esbuild.build({
+  entryPoints: [themePath],
+  bundle: true,
+  minify: true,
+  outfile: path.join("themes", fileName),
+}).then(() => console.log(`⚡ ${fileName} built! ⚡`))
+  .catch(() => process.exit(1));
